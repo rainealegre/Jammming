7483e7590196a4ce00ad0b07c75f5cdffbd5fdf3
rainealegre <raine_alegre@yahoo.com>
2018-06-12 13:58:36 +0800

    made changes

---
+++ src/Components/Playlist/Playlist.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './Playlist.css';
+
+import TrackList from '../TrackList/TrackList';
+
+class Playlist extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleNameChange = this.handleNameChange.bind(this);
+    }
+
+    handleNameChange(event) {
+        this.props.onNameChange(event.target.value);
+    }
+
+    render() {
+        return(
+            <div className="Playlist">
+                <input value={this.props.name} onChange={this.handleNameChange}/>
+                <TrackList 
+                    tracks={this.props.tracks}
+                    onRemove={this.props.onRemove}
+                    isRemoval={false}
+                />
+                <a className="Playlist-save" onClick={this.props.onSave}>SAVE TO SPOTIFY</a>
+            </div>
+        );
+    }
+}
+
+export default Playlist;
---
+++ src/Components/SearchBar/SearchBar.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './SearchBar.css';
+
+class SearchBar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { term: '' };
+        this.handleTermChange = this.handleTermChange.bind(this);
+    }
+
+    search() {
+        this.props.onSearch(this.state.term);
+    }
+
+    handleTermChange(event) {
+        this.setState({ term: event.target.value });
+    }
+
+    render() {
+        return(
+            <div className="SearchBar">
+                <input placeholder="Enter a Song, Album, or Artist" onChange={this.handleTermChange}/>
+                <a>SEARCH</a>
+            </div>
+        );
+    }
+}
+
+export default SearchBar;
