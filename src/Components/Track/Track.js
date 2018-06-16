baa756654b4a604fd9c849b2d744ff5868612008
rainealegre <raine_alegre@yahoo.com>
2018-06-16 21:33:41 +0800

    added icons next to track-information

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -38,6 +38,9 @@ class Track extends React.Component {
     render() {
         return(
             <div className="Track">
+                <div className="Track-controls">
+                    <img src="./play.png" />
+                </div>
                 <div className="Track-information">
                     <h3>{this.props.track.name}</h3>
                     <p>{this.props.track.artist} | {this.props.track.album}</p>
