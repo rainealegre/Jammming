d875a83c8e2fc43de67be2a1a59a82ef9b3d7403
rainealegre <raine_alegre@yahoo.com>
2018-06-20 09:40:43 +0800

    replaced redirect url

---
+++ src/Secret.js
@@ -0,0 +1,3 @@
+let client_Id = 'a0c292eb872f4ce98655f5bfc373dbe7';
+
+export default client_Id;
--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -5,8 +5,10 @@ const spotifySearchAPI = 'https://api.spotify.com/v1/search';
 const spotifyUserProfileAPI = 'https://api.spotify.com/v1/me';
 const spotifyPlaylistAPI = 'https://api.spotify.com/v1/users/${userId}/playlists';
 const spotifyPlaylistTracksAPI = 'https://api.spotify.com/v1/users/${userId}/playlists/${playlistId}/tracks';
+const redirectUrl = 'https://jammmingwithspotify.firebaseapp.com/';
+
+/* surge url */
 //const redirectUrl = 'http://jammmingwithspotify.surge.sh/';
-const redirectUrl = 'http://localhost:3000/';
 
 let accessToken;
 let expiresIn;
